86c5179ec9d696c3a984a34744b8c7d187864d35
akshayshaerma.2993@gmail.com <akshaysharma.2993@gmail.com>
2022-05-24 16:15:11 +0530

    Welcome message added!

---
+++ EmployeeWage.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.Java_Day8;
+
+public class EmployeeWage {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wage Computation Program using OOP Concept");
+    }
+}
