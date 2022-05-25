77d9c46b47a678e5008e2c3ea2c13d8ad4850ca5
akshayshaerma.2993@gmail.com <akshaysharma.2993@gmail.com>
2022-05-25 16:06:01 +0530

    Added UC-3:Added Part Time Employee and wage.

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,22 +3,28 @@ package com.bridgelabz.Java_Day8;
 public class EmployeeWage {
     static final int IS_ABSENT = 0;
     static final int IS_PRESENT = 1;
+    static final int IS_PART_TIME = 2;
     static final int WAGE_PER_HOUR = 20;
     static final int FULL_DAY_HOUR = 8;
+    static final int PART_TIME_HOUR = 4;
     static int empHrs = 0;
     static int dailyWage = 0;
-    int empCheck = (int)Math.floor(Math.random()*2);
+    int empCheck = (int)Math.floor(Math.random()*3);
     public void attendanceCheck(){
         if(empCheck == IS_ABSENT){
             System.out.println("Employee is Absent!");
-        }else{
+        }else if(empCheck == IS_PRESENT) {
             System.out.println("Employee is Present!");
+        }else {
+            System.out.println("Employee is Part-time!");
         }
     }
 
     public void calcDailyWage() {
-        if (empCheck != IS_ABSENT) {
+        if (empCheck == IS_PRESENT) {
             empHrs = FULL_DAY_HOUR;
+        } else if (empCheck == IS_PART_TIME) {
+            empHrs = PART_TIME_HOUR;
         }
         dailyWage += (WAGE_PER_HOUR * empHrs);
         System.out.println("Employee wage Today: " + dailyWage);
