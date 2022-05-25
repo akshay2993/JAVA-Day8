8f619546bc157beb6a0527ae8dc3cfd125b8d79b
akshayshaerma.2993@gmail.com <akshaysharma.2993@gmail.com>
2022-05-25 15:58:05 +0530

    Added UC-2:Added Daily wage calculation.

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,15 +3,25 @@ package com.bridgelabz.Java_Day8;
 public class EmployeeWage {
     static final int IS_ABSENT = 0;
     static final int IS_PRESENT = 1;
-    int empCheck = (int)Math.floor(Math.random()*3);
+    static final int WAGE_PER_HOUR = 20;
+    static final int FULL_DAY_HOUR = 8;
+    static int empHrs = 0;
+    static int dailyWage = 0;
+    int empCheck = (int)Math.floor(Math.random()*2);
     public void attendanceCheck(){
         if(empCheck == IS_ABSENT){
             System.out.println("Employee is Absent!");
-        }else if(empCheck == IS_PRESENT) {
+        }else{
             System.out.println("Employee is Present!");
-        }else {
-            System.out.println("Employee is Part-time!");
         }
+    }
+
+    public void calcDailyWage() {
+        if (empCheck != IS_ABSENT) {
+            empHrs = FULL_DAY_HOUR;
+        }
+        dailyWage += (WAGE_PER_HOUR * empHrs);
+        System.out.println("Employee wage Today: " + dailyWage);
     }
 
     public static void main(String[] args) {
@@ -21,5 +31,8 @@ public class EmployeeWage {
 
         System.out.println("Attendance Check:");
         empWage.attendanceCheck();
+
+        System.out.println("Calculating Daily Wage: ");
+        empWage.calcDailyWage();
     }
 }
