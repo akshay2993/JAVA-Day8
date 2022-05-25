1b9eef36bcce67ef30ca536de7a323bd513cb2fa
akshayshaerma.2993@gmail.com <akshaysharma.2993@gmail.com>
2022-05-25 15:42:07 +0530

    Added UC-1:Employee Attendance Check

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,9 +1,25 @@
 package com.bridgelabz.Java_Day8;
 
 public class EmployeeWage {
+    static final int IS_ABSENT = 0;
+    static final int IS_PRESENT = 1;
+    int empCheck = (int)Math.floor(Math.random()*3);
+    public void attendanceCheck(){
+        if(empCheck == IS_ABSENT){
+            System.out.println("Employee is Absent!");
+        }else if(empCheck == IS_PRESENT) {
+            System.out.println("Employee is Present!");
+        }else {
+            System.out.println("Employee is Part-time!");
+        }
+    }
 
     public static void main(String[] args) {
+        EmpWage empWage = new EmpWage();
 
         System.out.println("Welcome to Employee Wage Computation Program using OOP Concept");
+
+        System.out.println("Attendance Check:");
+        empWage.attendanceCheck();
     }
 }
